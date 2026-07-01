af9f22ee7d1d70e6baa02ef02095cd5e2220013c
sernadyaa <nadya.sergeeva.08.08@mail.ru>
2026-07-01 15:13:52 +0300

    Создала main.py для запуска приложения

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QFont
+from ui_main import MainWindow
+
+def main():
+    app = QApplication(sys.argv)
+    app.setFont(QFont('Verdana', 12)
+    app.setApplicationName('Менеджер рецептов')
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
